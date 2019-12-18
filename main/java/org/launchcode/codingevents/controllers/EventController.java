ee746d898b083ae127ddeeb96fa66d21e0f31c3d
SarahF <sarahfrerichs@gmail.com>
2019-12-17 23:30:22 -0600

    11.6.4 added form handler method

--- main/java/org/launchcode/codingevents/controllers/EventController.java
+++ main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,18 +14,28 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String > events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("First Coding Event");
-        events.add("Second Coding Event");
-        events.add("3rd Coding Event");
-        return "events/index";
+        //code for static list:
+//        List<String> events = new ArrayList<>();
+//        events.add("First Coding Event");
+//        events.add("Second Coding Event");
+//        events.add("3rd Coding Event");
+        model.addAttribute("events", events);
+        return "index";
     }
 
     @GetMapping("create") //lives at /events/create
     public String renderCreateEventForm() {
         return "create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
     }
 
 }
